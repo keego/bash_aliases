3abc06b7ea9986751e34354c713c13766642d154
Keegan Farley <kfarley@axon.com>
2021-03-15 22:38:52 -0700

    Support pushing updates when working directory is clean

--- push-update.sh
+++ push-update.sh
@@ -5,6 +5,6 @@ NOW_BRRANCH="update-$(date '+%Y-%m-%d--%H-%M-%S')"
 
 git add -A &&
 git checkout -b "$NOW_BRRANCH" &&
-git commit -m "Updating as of $NOW" &&
+git commit -m "Updating as of $NOW" --allow-empty &&
 git push origin "$NOW_BRRANCH" &&
 open "https://github.com/keego/bash_aliases/pull/new/$NOW_BRRANCH"
