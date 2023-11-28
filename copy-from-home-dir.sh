134d0f27f6bc7713e74ea05b61e9c23775368a13
Keegan Farley <keegansfarley@gmail.com>
2023-11-27 18:49:25 -0800

    Implement basic caching

--- copy-from-home-dir.sh
+++ copy-from-home-dir.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+set -e
+
+CACHE_DIR="./tmp-cache"
+
+setup-cache () {
+  if [ -e "$CACHE_DIR" ] ; then
+   rm -r "$CACHE_DIR"
+  fi
+  mkdir "$CACHE_DIR"
+  echo " (caching local files to $CACHE_DIR)"
+}
+
 copy-from-home () {
   if [ -e ~/"$1" ] ; then
+    # cache before overwriting
+    cp -r ./"$1" "$CACHE_DIR/$1"
+    # remove and copy
     rm -r ./"$1"
-    cp -ivr ~/"$1" ./
+    cp -vr ~/"$1" ./
   else
     echo "skipping" ~/"$1"
   fi
-  echo
 }
 
-echo "interactively copying files..."
+echo "pulling files from home dir..."
+setup-cache
 echo
 copy-from-home .bash_aliases
 copy-from-home .bash_env
--- copy-to-home-dir.sh
+++ copy-to-home-dir.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
+set -e
+
+CACHE_DIR="./tmp-cache"
+
+setup-cache () {
+  if [ -e "$CACHE_DIR" ] ; then
+   rm -r "$CACHE_DIR"
+  fi
+  mkdir "$CACHE_DIR"
+  echo " (caching old home files to $CACHE_DIR)"
+}
+
 copy-to-home () {
   if [ -e ./"$1" ] ; then
+    # cache before overwriting
+    cp -r ~/"$1" "$CACHE_DIR/$1"
+    # remove and copy
+
     # if the source and destination aren't both files or directories
     # then delete the destination to prevent copying from failing
     if [ "$(file -b ~/$1)" != "$(file -b ./$1)" ] ; then
       rm -r ~/"$1"
     fi
-    cp -ivr ./"$1" ~/
+    cp -vr ./"$1" ~/
   else
     echo "skipping" ~/"$1"
   fi
-  echo
 }
 
-echo "interactively copying files..."
+echo "pushing files to home dir..."
+setup-cache
 echo
 copy-to-home .bash_aliases
 copy-to-home .bash_env
