932923b653a06ac28d019e3203788563349be0cb
Keegan Farley <keegansfarley@gmail.com>
2023-11-28 17:38:14 -0800

    Updating as of 2023-11-28 17:38:14 Changes to be committed:   (use "git restore --staged <file>..." to unstage) 	modified:   copy-to-home-dir.sh

--- copy-to-home-dir.sh
+++ copy-to-home-dir.sh
@@ -14,14 +14,16 @@ setup-cache () {
 
 copy-to-home () {
   if [ -e ./"$1" ] ; then
-    # cache before overwriting
-    cp -r ~/"$1" "$CACHE_DIR/$1"
-    # remove and copy
+    if [ -e ~/"$1" ] ; then
+      # cache before overwriting
+      cp -r ~/"$1" "$CACHE_DIR/$1"
+      # remove and copy
 
-    # if the source and destination aren't both files or directories
-    # then delete the destination to prevent copying from failing
-    if [ "$(file -b ~/$1)" != "$(file -b ./$1)" ] ; then
-      rm -r ~/"$1"
+      # if the source and destination aren't both files or directories
+      # then delete the destination to prevent copying from failing
+      if [ "$(file -b ~/$1)" != "$(file -b ./$1)" ] ; then
+        rm -r ~/"$1"
+      fi
     fi
     cp -vr ./"$1" ~/
   else
