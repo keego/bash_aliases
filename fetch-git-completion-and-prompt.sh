9eeb750e5b8358c8e143d7e5b3d8c49b7ea30c41
Keegan Farley <keegansfarley@gmail.com>
2023-12-13 18:04:14 -0800

    Update script fetcher to include git-completion.zsh

--- fetch-git-completion-and-prompt.sh
+++ fetch-git-completion-and-prompt.sh
@@ -2,8 +2,11 @@
 
 # grab files
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > .git-completion.bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh > .git-completion.zsh
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > .git-prompt.sh
 
 # move them to ~, prompting to continue on overwrite
 mv -i .git-completion.bash ~
+mkdir ~/.zsh
+mv -i .git-completion.zsh ~/.zsh/git
 mv -i .git-prompt.sh ~
